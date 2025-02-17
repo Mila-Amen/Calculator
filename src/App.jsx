7182ca2334f55a4a5394714cf2218bdb2c42d1db
Mila-Amen <million.degefu@dci-student.org>
2025-02-17 10:00:37 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/no-unknown-property */
+import React from "react";
+import Calculator from "./components/Calculator";
+
+function App() {
+  return (
+    <div className='text-red'>
+      <Calculator />
+    </div>
+  );
+}
+
+export default App;
